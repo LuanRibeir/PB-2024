0b1c994535df67285e1cccd5a1c40700458147f2
LuanRibeir <luaneribeiroalmeida@gmail.com>
2024-05-18 18:40:48 -0300

    Initial commit

---
+++ src/main/java/com/luanr/pb2024/Pb2024Application.java
@@ -0,0 +1,13 @@
+package com.luanr.pb2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pb2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pb2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/luanr/pb2024/Pb2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luanr.pb2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pb2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
