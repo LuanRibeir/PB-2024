0345d995093789bcae74a6faa502fdf2d62197a4
LuanRibeir <luaneribeiroalmeida@gmail.com>
2024-05-20 17:29:19 -0300

    Implement user model

---
+++ src/main/java/com/luanr/pb2024/model/User.java
@@ -0,0 +1,24 @@
+package com.luanr.pb2024.model;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @MongoId
+    private ObjectId id;
+
+    private String name;
+
+    private String profilePicture;
+}
